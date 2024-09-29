bf2b087f4a3c850cb9413a237322f9e2e9416385
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-09-30 00:12:51 +0530

    all deleted

--- ball-to-ball/src/app/custom_pipe/floor-value.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FloorValuePipe } from './floor-value.pipe';
-
-describe('FloorValuePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FloorValuePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
